3e062986a4df54fa795d632339e0143f1d9ff051
Alex <ahowe23@my.bcit.ca>
2024-11-02 15:11:25 -0700

    Tried to add maps to firestore and link with JS

--- scripts/main.js
+++ scripts/main.js
@@ -25,19 +25,39 @@ getNameFromAuth(); //run the function
 
 // Function to read the quote of the day from the Firestore "quotes" collection
 // Input param is the String representing the day of the week, aka, the document name
-function readQuote(day) {
-  db.collection("quotes")
-    .doc(day) //name of the collection and documents should matach excatly with what you have in Firestore
-    .onSnapshot((dayDoc) => {
+function readMap(map) {
+  db.collection("floor plans").doc(map) //name of the collection and documents should matach excatly with what you have in Firestore
+    .onSnapshot(mapDoc => {
       //arrow notation
-      console.log("current document data: " + dayDoc.data()); //.data() returns data object
-      document.getElementById("quote-goes-here").innerHTML =
-        dayDoc.data().quote; //using javascript to display the data on the right place
+      console.log("current document data: " + mapDoc.data()); //.data() returns data object
+      document.getElementById("floor-plan-goes-here").innerHTML = mapDoc.data().floorplan; //using javascript to display the data on the right place
 
       //Here are other ways to access key-value data fields
       //$('#quote-goes-here').text(dayDoc.data().quote);         //using jquery object dot notation
       //$("#quote-goes-here").text(dayDoc.data()["quote"]);      //using json object indexing
       //document.querySelector("#quote-goes-here").innerHTML = dayDoc.data().quote;
+    }, (error) => {
+      console.log("Error calling onSnapshot", error);
     });
 }
-readQuote("tuesday"); //calling the function
+readMap("floor1"); //calling the function
+
+
+// Function to read the quote of the day from the Firestore "quotes" collection
+// Input param is the String representing the day of the week, aka, the document name
+function readQuote(day) {
+  db.collection("quotes").doc(day)                                                         //name of the collection and documents should matach excatly with what you have in Firestore
+    .onSnapshot(dayDoc => {                                                              //arrow notation
+      console.log("current document data: " + dayDoc.data());                          //.data() returns data object
+      document.getElementById("quote-goes-here").innerHTML = dayDoc.data().quote;      //using javascript to display the data on the right place
+
+      //Here are other ways to access key-value data fields
+      //$('#quote-goes-here').text(dayDoc.data().quote);         //using jquery object dot notation
+      //$("#quote-goes-here").text(dayDoc.data()["quote"]);      //using json object indexing
+      //document.querySelector("#quote-goes-here").innerHTML = dayDoc.data().quote;
+
+    }, (error) => {
+      console.log("Error calling onSnapshot", error);
+    });
+}
+readQuote("tuesday");        //calling the function
