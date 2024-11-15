d308a07dde7593e8d2f3ad5a640a087081a24709
nillnought <gglaim@my.bcit.ca>
2024-11-14 21:49:42 -0800

    fix map centering and add floor button click function

--- scripts/main.js
+++ scripts/main.js
@@ -138,4 +138,18 @@ function imageZoom(imgID, resultID) {
 // Initiate zoom effect:
 imageZoom("mapimage2", "myresult");
 
-
+var count = 0;
+function floorList(){
+  count++;
+  listFloors = document.querySelector(".dropdown-content");
+  arrow = document.querySelector(".arrowdown");
+  if (count == 1){
+    listFloors.style = "display: block";
+    arrow.style = "display: none";
+  }
+  else if (count == 2){
+    listFloors.style = "display:none";
+    arrow.style = "display:block";
+    count = 0;
+  }
+}
