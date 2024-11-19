8eba1ee8935da5d31ae3da54ec8acd945245dc75
nillnought <gglaim@my.bcit.ca>
2024-11-19 11:24:17 -0800

    add dates to events

--- scripts/main.js
+++ scripts/main.js
@@ -61,11 +61,13 @@ function displayCardsDynamically(collection) {
         var title = doc.data().name;       // get value of the "name" key
         var details = doc.data().description;
         var docID = doc.id;
+        var date = doc.data().date;
         let newcard = cardTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
 
         //update title and text and image
         newcard.querySelector('#eventTitle').innerHTML = title;
         newcard.querySelector('#eventBriefDescription').innerHTML = details;
+        newcard.querySelector("#date").innerHTML = date;
         newcard.querySelector('#eventButton').href = "event.html?docID="+docID;
         document.getElementById("eventItemList").appendChild(newcard);
 
