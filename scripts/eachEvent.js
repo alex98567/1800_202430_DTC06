3fe2edccaec15282389b1265b15fe3bb318d7d65
nillnought <gglaim@my.bcit.ca>
2024-11-27 21:38:56 -0800

    add displaying an event is favorited on event pages

--- scripts/eachEvent.js
+++ scripts/eachEvent.js
@@ -15,46 +15,98 @@ function displayEventInfo() {
 
             document.getElementById("eventTitleName").innerHTML = eventName;
             document.getElementById("EventText").innerHTML = eventDescription;
-            if (eventImg) {
-                document.querySelector("#eventImg").src = eventImage;
-            }
+            firebase.auth().onAuthStateChanged(user =>{
+                userID = db.collection("users").doc(user.uid);
+                userID.get().then(userDoc =>{
+                  var favorited = userDoc.data().favorites;
+                  if (favorited.includes(ID)){
+                    document.getElementById("AddFavorite").innerHTML = "Remove from Favorites"
+                  }
+                  else{
+                    document.getElementById("AddFavorite").innerHTML = "Add to Favorites"
+                  }
+                })
+              })
         });
 }
 displayEventInfo();
 
-counter = 0
+// counter = 0
+// function addToFavorites() {
+//     firebase.auth().onAuthStateChanged(user => {
+//         if (user) {
+//             counter++;
+//             let params = new URL(window.location.href);
+//             let ID = params.searchParams.get("docID");
+//             userID = db.collection("users").doc(user.uid);
+//             favoriteButton = document.getElementById("AddFavorite");
+//             if (counter == 1) {
+//                 userID.update({
+//                     favorites: firebase.firestore.FieldValue.arrayUnion(ID)
+//                 })
+//                     .then(() => {
+//                         if (favoriteButton) {
+//                             favoriteButton.innerHTML = "Remove from Favorites"
+//                         }
+//                         alert("Added Event to Favorites!");
+//                     })
+//             }
+//             else if (counter == 2) {
+//                 userID.update({
+//                     favorites: firebase.firestore.FieldValue.arrayRemove(ID)
+//                 })
+//                     .then(() => {
+//                         counter = 0;
+//                         console.log(counter)
+//                         if (favoriteButton) {
+//                             favoriteButton.innerHTML = "Add to Favorites"
+//                         }
+//                     })
+//             }
+
+//         }
+//         else {
+//             alert("Error! No user signed in!")
+//         }
+
+//     })
+
+// }
 function addToFavorites() {
     firebase.auth().onAuthStateChanged(user => {
         if (user) {
-            counter++;
             let params = new URL(window.location.href);
             let ID = params.searchParams.get("docID");
             userID = db.collection("users").doc(user.uid);
             favoriteButton = document.getElementById("AddFavorite");
-            if (counter == 1) {
-                userID.update({
-                    favorites: firebase.firestore.FieldValue.arrayUnion(ID)
-                })
-                    .then(() => {
-                        if (favoriteButton) {
-                            favoriteButton.innerHTML = "Remove from Favorites"
-                        }
-                        alert("Added Event to Favorites!");
-                    })
-            }
-            else if (counter == 2) {
+            firebase.auth().onAuthStateChanged(user =>{
+            userID = db.collection("users").doc(user.uid);
+            userID.get().then(userDoc =>{
+              var favorited = userDoc.data().favorites;
+              if (favorited.includes(ID)) {
                 userID.update({
                     favorites: firebase.firestore.FieldValue.arrayRemove(ID)
                 })
-                    .then(() => {
-                        counter = 0;
-                        console.log(counter)
+                    .then(async() => {
                         if (favoriteButton) {
-                            favoriteButton.innerHTML = "Add to Favorites"
+                          console.log("removed")
+                          location.reload(true)
                         }
                     })
             }
-
+            else{
+                userID.update({
+                    favorites: firebase.firestore.FieldValue.arrayUnion(ID)
+                })
+                    .then(async() => {
+                        if (favoriteButton) {
+                          console.log("added")
+                          location.reload(true)
+                        }
+                    })
+            }
+            })
+          })
         }
         else {
             alert("Error! No user signed in!")
