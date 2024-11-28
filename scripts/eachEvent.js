6de9812bdc84343f07707865f210bf0f1ba3fd6c
nillnought <gglaim@my.bcit.ca>
2024-11-27 21:39:51 -0800

    clean up unused code

--- scripts/eachEvent.js
+++ scripts/eachEvent.js
@@ -31,47 +31,6 @@ function displayEventInfo() {
 }
 displayEventInfo();
 
-// counter = 0
-// function addToFavorites() {
-//     firebase.auth().onAuthStateChanged(user => {
-//         if (user) {
-//             counter++;
-//             let params = new URL(window.location.href);
-//             let ID = params.searchParams.get("docID");
-//             userID = db.collection("users").doc(user.uid);
-//             favoriteButton = document.getElementById("AddFavorite");
-//             if (counter == 1) {
-//                 userID.update({
-//                     favorites: firebase.firestore.FieldValue.arrayUnion(ID)
-//                 })
-//                     .then(() => {
-//                         if (favoriteButton) {
-//                             favoriteButton.innerHTML = "Remove from Favorites"
-//                         }
-//                         alert("Added Event to Favorites!");
-//                     })
-//             }
-//             else if (counter == 2) {
-//                 userID.update({
-//                     favorites: firebase.firestore.FieldValue.arrayRemove(ID)
-//                 })
-//                     .then(() => {
-//                         counter = 0;
-//                         console.log(counter)
-//                         if (favoriteButton) {
-//                             favoriteButton.innerHTML = "Add to Favorites"
-//                         }
-//                     })
-//             }
-
-//         }
-//         else {
-//             alert("Error! No user signed in!")
-//         }
-
-//     })
-
-// }
 function addToFavorites() {
     firebase.auth().onAuthStateChanged(user => {
         if (user) {
