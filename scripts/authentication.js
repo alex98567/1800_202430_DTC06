d3abe2b7a0b0f1d7185f7e64b543e33be81de424
nillnought <gglaim@my.bcit.ca>
2024-12-06 09:21:23 -0800

    comment out profile picture

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -4,13 +4,15 @@ var ui = new firebaseui.auth.AuthUI(firebase.auth());
 var uiConfig = {
   callbacks: {
     signInSuccessWithAuthResult: function (authResult, redirectUrl) {
-      var user = authResult.user;                            // get the user object from the Firebase authentication database
+      var user = authResult.user;                       // get the user object from the Firebase authentication database
       if (authResult.additionalUserInfo.isNewUser) {         //if new user
         db.collection("users").doc(user.uid).set({         //write to firestore. We are using the UID for the ID in users collection
           name: user.displayName,                    //"users" collection
           email: user.email,                         //with authenticated user's ID (user.uid)
           country: "Canada",                      //optional default profile info      
-          school: "BCIT"                          //optional default profile info
+          school: "BCIT",                          //optional default profile info
+          favorites: [],
+          profile: "https://upload.wikimedia.org/wikipedia/commons/6/6f/BCIT_logo.svg"
         }).then(function () {
           console.log("New user added to firestore");
           window.location.assign("home.html");       //re-direct to home.html after signup
